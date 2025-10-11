4e9b2c7bcec39feeef0f3f7b97a17c4ab8112f53
Phlaveeoh <flavio.merli.tv.03@gmail.com>
2025-10-11 10:40:58 +0200

    Terminarte le api ed il lavoro sul server

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -1,3 +1,35 @@
-exports.getCibi = async (req, res) => { };
+const pool = require("../servizi/servizioDB");
 
-exports.getGiochi = async (req, res) => { };
+exports.getCibi = async (req, res) => {
+    try {
+        const [cibi] = await pool.promise().execute("SELECT * FROM cibi");
+        return res.status(200).json({
+            success: true,
+            message: "Cibi recuperati con successo",
+            cibi: cibi
+        });
+    } catch (error) {
+        console.error("Errore durante il recupero dei cibi:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Errore del server"
+        });
+    }
+};
+
+exports.getGiochi = async (req, res) => {
+    try {
+        const [giochi] = await pool.promise().execute("SELECT * FROM giochi");
+        return res.status(200).json({
+            success: true,
+            message: "Giochi recuperati con successo",
+            giochi: giochi
+        });
+    } catch (error) {
+        console.error("Errore durante il recupero dei giochi:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Errore del server"
+        });
+    }
+};
